b1028b32274c5e898cae89d3c7c53301bb7fcfa7
archiemeng <myt1996037@gmail.com>
2018-05-13 01:01:17 +0800

    1. use argparse to handle command line inputs 2. change xrange to range

--- cl-waifu2x.py
+++ cl-waifu2x.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
 import pyopencl as cl
@@ -7,23 +7,37 @@ from scipy import misc
 from PIL import Image
 
 from cl_simple import CLNN_Simple
+import argparse
+import os
 
-infile, outfile, modelpath = sys.argv[1:]
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--input', '-i', default='sample.jpg')
+arg_parser.add_argument('--output', '-o', default=None)
+arg_parser.add_argument('--model_file', '-m', default='models/scale2.0x_model.json')
+args = arg_parser.parse_args()
+
+dirname = os.path.dirname(os.path.realpath(__file__))
+infile = args.input
+default_outfile_name = '.'.join(infile.split('.')[:-1]) + '(cl_2x)'
+outfile = args.output or default_outfile_name + '.' + infile.split('.')[-1]
+modelpath = args.model_file
+
 scale = "scale" in modelpath
 
-ctx = cl.create_some_context()
+ctx = cl.create_some_context(interactive=True)
 nn = CLNN_Simple(ctx, modelpath)
 
 im = Image.open(infile).convert("YCbCr")
 
 if scale:
-    im = im.resize((2*im.size[0], 2*im.size[1]), resample=Image.NEAREST)
+    im = im.resize((2*im.size[0], 2*im.size[1]), resample=Image.BICUBIC)
 
 im = misc.fromimage(im)
 luma = im[:,:,0]
 #misc.toimage(luma, mode="L").save("luma_"+outfile)
 
 in_plane = np.clip(luma.astype("float32") / 255.0, 0, 1)
+
 
 def progress(frac):
     sys.stderr.write("\r%.1f%%..." % (100 * frac))
