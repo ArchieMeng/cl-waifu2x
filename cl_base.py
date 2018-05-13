4be7704f032ef0d9064a3c6c2e56356961aff4d9
archiemeng <myt1996037@gmail.com>
2018-05-13 16:41:43 +0800

    move source code of convolve_many to convolve_many.c

--- cl_base.py
+++ cl_base.py
@@ -12,39 +12,8 @@ class CLNN_Base(object):
         self.total_time = 0
         self.total_pixels = 0
         self.bw, self.bh = 128, 128
-        self.prg = cl.Program(self.ctx, """
-        __kernel void convolve_many(
-            const unsigned int bx,
-            const unsigned int by,
-            const unsigned int num_inputs,
-            const unsigned int num_outputs,
-            global const float *in,
-            global const float *k,
-            global const float *bias,
-            global float *out)
-        {
-            int bs = bx*by;
-            int xy = get_global_id(0) + get_global_id(1) * bx;
-            int z = get_global_id(2);
-
-            float acc = 0;
-            int k_off = z * num_inputs * 9;
-            for (int i = 0; i < num_inputs; i++) {
-                int i_off = xy + i*bs;
-                acc += in[i_off + 0*bx + 0] * k[k_off + i*9 + 0];
-                acc += in[i_off + 0*bx + 1] * k[k_off + i*9 + 1];
-                acc += in[i_off + 0*bx + 2] * k[k_off + i*9 + 2];
-                acc += in[i_off + 1*bx + 0] * k[k_off + i*9 + 3];
-                acc += in[i_off + 1*bx + 1] * k[k_off + i*9 + 4];
-                acc += in[i_off + 1*bx + 2] * k[k_off + i*9 + 5];
-                acc += in[i_off + 2*bx + 0] * k[k_off + i*9 + 6];
-                acc += in[i_off + 2*bx + 1] * k[k_off + i*9 + 7];
-                acc += in[i_off+ 2*bx + 2] * k[k_off + i*9 + 8];
-            }
-            acc += bias[z];
-            out[xy + z*bs] = acc - 0.9 * fmin(acc, 0);
-        }
-        """).build()
+        with open("convolve_many.c", 'r') as fp:
+            self.prg = cl.Program(self.ctx, fp.read()).build()
 
     def load(self, model_file):
         mf = cl.mem_flags
